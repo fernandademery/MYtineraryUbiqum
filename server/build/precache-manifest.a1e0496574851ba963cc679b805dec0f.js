54c5504961f69ee40a1ab3dfdd0d0c3e908fa891
fernandademery <nandademery2@hotmail.com>
2021-06-04 18:09:23 +0200

    Adjusting details for deployment

---
+++ server/build/precache-manifest.a1e0496574851ba963cc679b805dec0f.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e84bb72a28053faab191043322a7bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5a740eafade9928f645",
+    "url": "/static/css/2.237a6266.chunk.css"
+  },
+  {
+    "revision": "387975624cfd652ceed2",
+    "url": "/static/css/main.6da5c0cd.chunk.css"
+  },
+  {
+    "revision": "c5a740eafade9928f645",
+    "url": "/static/js/2.701a26b3.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.701a26b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "387975624cfd652ceed2",
+    "url": "/static/js/main.ea557026.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "a227b9b34c73ccb652b542ab0b57d717",
+    "url": "/static/media/MYtineraryLogo.a227b9b3.png"
+  },
+  {
+    "revision": "1f6126cdf48f90d9cb150c82206a6b7a",
+    "url": "/static/media/Star_Empty-512.1f6126cd.png"
+  },
+  {
+    "revision": "a64f98187c0b416fa03d5f05beed97d6",
+    "url": "/static/media/barcelona2.a64f9818.jpg"
+  },
+  {
+    "revision": "37530144c8a45936dd867ae0d70227f4",
+    "url": "/static/media/berlin.37530144.jpg"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/static/media/blank-profile-picture.5a021ab9.png"
+  },
+  {
+    "revision": "f1e2a077606c1cfa5884fae742e37428",
+    "url": "/static/media/buenos-aires.f1e2a077.jpg"
+  },
+  {
+    "revision": "16fd0ef3e7fa969db6599edd46ee4033",
+    "url": "/static/media/circled-right-2.16fd0ef3.png"
+  },
+  {
+    "revision": "bfb4da324c19af90c766dbbf07df1bad",
+    "url": "/static/media/lisboa.bfb4da32.jpg"
+  },
+  {
+    "revision": "a5d6e8bd838133843d7c9f75ab458c15",
+    "url": "/static/media/medellin.a5d6e8bd.jpg"
+  },
+  {
+    "revision": "79696b61711a619a244e7c160fdb2ade",
+    "url": "/static/media/paris.79696b61.jpg"
+  },
+  {
+    "revision": "bc5a93804c41aafd5f653b12b932c2e2",
+    "url": "/static/media/recife07.bc5a9380.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "bdff768ad14e08c859d98be2e4844fcf",
+    "url": "/static/media/rio.bdff768a.jpg"
+  },
+  {
+    "revision": "54301740b21aa8a682bfe70fd145d816",
+    "url": "/static/media/roma2.54301740.jpg"
+  },
+  {
+    "revision": "4e4b371ed5b5c936a01937d611285ab0",
+    "url": "/static/media/star-full.4e4b371e.png"
+  },
+  {
+    "revision": "995dab394672cb62c4d4e2ec8a37949e",
+    "url": "/static/media/sydney.995dab39.jpg"
+  },
+  {
+    "revision": "62580ed1423bd1213ac117bf90012ded",
+    "url": "/static/media/thessaloniki2.62580ed1.jpg"
+  },
+  {
+    "revision": "451ba9974324ba674a67a36f1bcb1b10",
+    "url": "/static/media/viena.451ba997.jpg"
+  }
+]);
